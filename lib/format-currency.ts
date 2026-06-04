ce80f33a990dba7dbd0fd71264b0311980c936a5
Ezequiel Rangel <chekelinho@gmail.com>
2026-06-03 21:58:56 -0700

    Use es-MX locale in formatCurrency

--- lib/format-currency.ts
+++ lib/format-currency.ts
@@ -1,5 +1,5 @@
 export function formatCurrency(value: number): string {
-  return value.toLocaleString("en-US", {
+  return value.toLocaleString("es-MX", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
